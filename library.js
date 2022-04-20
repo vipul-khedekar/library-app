0dd3e6bba802beb0f0564a89f74e2b77a7350f8e
vipul-khedekar <3kvipul@gmail.com>
2022-04-20 16:49:46 +0530

    queried necessary elements related to form inputs

--- library.js
+++ library.js
@@ -1,3 +1,9 @@
+const form = document.querySelector(`[data-form]`);
+const formTitle = document.querySelector(`[data-form-title]`)
+const formAuthor = document.querySelector(`[data-form-author]`)
+const formPrice = document.querySelector(`[data-form-price]`)
+const formStatus = document.querySelector(`[data-form-status]`)
+
 function BOOK(title, author, price, status) {
     this.title = title;
     this.author = author;
